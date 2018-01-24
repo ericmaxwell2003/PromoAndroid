74896d196c627902215c6daab0bc14d47e3c7d16
Eric Maxwell <ericmaxwell2003@gmail.com>
2018-01-24 10:30:39 -0500

    Add JavaDoc comment to ButtonTypeAdapter to explain its intent.

--- app/src/main/java/software/credible/abercrombiefitchkata/remote/ButtonTypeAdapter.java
+++ app/src/main/java/software/credible/abercrombiefitchkata/remote/ButtonTypeAdapter.java
@@ -10,6 +10,36 @@ import java.util.List;
 
 import software.credible.abercrombiefitchkata.dto.ButtonDto;
 
+/**
+ * Buttons may come as a JSON Array or as a single Button.  This type adapter
+ * allows PromotionDto to easily just get back a list of buttons, regardless of whether
+ * a single Button was returned or an array of Buttons.
+ *
+ * Examples:
+ * As a JSON Array
+ * {
+ *   "promotions": [{
+ *       "button": {
+ *           "target": "https://m.abercrombie.com",
+ *           "title": "Shop Now"
+ *       },...],
+ *       ...
+ *   }
+ *   ...
+ *  }
+ *
+ * As a single element
+ * {
+ *   "promotions": {
+ *       "button": {
+ *           "target": "https://m.hollisterco.com",
+ *           "title": "Shop Now"
+ *       },
+ *       ...
+ *   }
+ *   ...
+ *  }
+ */
 public class ButtonTypeAdapter implements JsonDeserializer<List<ButtonDto>> {
 
     public List<ButtonDto> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext ctx) {
